705e4a815d70aaa47c95c363e58ef5c7efc33f65
zinzinyou <zinzinyou@korea.ac.kr>
2024-07-29 08:33:40 +0900

    [Modify] 게시판

--- src/screens/Community/TodayBriefingWidget.tsx
+++ src/screens/Community/TodayBriefingWidget.tsx
@@ -6,7 +6,8 @@ import {
     TouchableOpacity,
   } from 'react-native';
   import React from 'react';
-  import ProgressBar from '../../ProgessBar.tsx';
+  // import ProgressBar from '../../ProgessBar.tsx';
+  import ProgressBar from '../../component/ProgessBar.tsx';
   import {Lecture} from '../timetable/TimetableTypes.tsx';
   
   const highlightColor = '#f91482';
@@ -49,28 +50,28 @@ import {
       </View>
     );
   }
-  function ExamProgress({progress}: {progress: number}) {
+  function ExamProgress({progress,text}: {progress: number,text:string}) {
     return (
       <View style={styles.item}>
-        <Text style={styles.text}>시험 관련 공지가 있었어요</Text>
-        <ProgressBar progress={progress} />
+        {/* <Text style={styles.text}>시험 관련 공지가 있었어요</Text> */}
+        <ProgressBar progress={progress} text={text}/>
       </View>
     );
   }
   
-  function AttendanceProgress({progress}: {progress: number}) {
+  function AttendanceProgress({progress,text}: {progress: number,text:string}) {
     return (
       <View style={styles.item}>
-        <Text style={styles.text}>출석 관련 공지가 있었어요</Text>
-        <ProgressBar progress={progress} />
+        {/* <Text style={styles.text}>출석 관련 공지가 있었어요</Text> */}
+        <ProgressBar progress={progress} text={text}/>
       </View>
     );
   }
-  function AssignmentProgress({progress}: {progress: number}) {
+  function AssignmentProgress({progress,text}: {progress: number,text:string}) {
     return (
       <View style={styles.item}>
-        <Text style={styles.text}>과제 관련 공지가 있었어요</Text>
-        <ProgressBar progress={progress} />
+        {/* <Text style={styles.text}>과제 관련 공지가 있었어요</Text> */}
+        <ProgressBar progress={progress} text={text}/>
       </View>
     );
   }
@@ -79,9 +80,9 @@ import {
     return (
       <View style={styles.container}>
         <BriefingHeader lectureName={lecture.name} />
-        <ExamProgress progress={60} />
-        <AttendanceProgress progress={45} />
-        <AssignmentProgress progress={90} />
+        <ExamProgress progress={60} text="시험 관련 공지가 있었어요"/>
+        <AttendanceProgress progress={45} text="출석 관련 공지가 있었어요"/>
+        <AssignmentProgress progress={90} text="과제 관련 공지가 있었어요"/>
       </View>
     );
   }
