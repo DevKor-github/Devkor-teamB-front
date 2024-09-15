61364d2119327e0d270ad07e2d64d5bb036eec57
zinzinyou <zinzinyou@korea.ac.kr>
2024-09-15 20:41:13 +0900

    [Fix] API URL 변경 및 env 파일 생성

---
+++ src/Events.tsx
@@ -0,0 +1,25 @@
+class EventListener {
+    listeners: {desc: string; callback: Function}[];
+  
+    constructor() {
+      this.listeners = [];
+    }
+  
+    emit(target: string, value: any) {
+      this.listeners.forEach(
+        ({desc, callback}) => desc === target && callback(value),
+      );
+    }
+  
+    addListener(desc: string, callback: Function) {
+      this.listeners.push({desc: desc, callback: callback});
+      return this;
+    }
+  
+    removeListener(desc: string) {
+      this.listeners = this.listeners.filter(listener => listener.desc !== desc);
+    }
+  }
+  
+  const PointEventHandler = new EventListener();
+  export {PointEventHandler};
